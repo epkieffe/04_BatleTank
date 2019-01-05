e75154d3dd6e6cd4633fe6eaebe12d81f42edd35
epkieffe <epkieffe@ncsu.edu>
2019-01-05 11:58:12 -0500

    Simplified tank collision. Created classes for track (and eventual thruster).

---
+++ BattleTank/Source/BattleTank/Public/TankThruster.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/StaticMeshComponent.h"
+#include "TankThruster.generated.h"
+
+/**
+ * 
+ */
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class BATTLETANK_API UTankThruster : public UStaticMeshComponent
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
